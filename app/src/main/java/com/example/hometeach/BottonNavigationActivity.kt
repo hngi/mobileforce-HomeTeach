789c6a1ed55be0fe3c58a0c57e354a9091981cfe
mikail <mikailkyusuf@gmail.com>
2020-06-20 22:28:35 +0100

    Changed the name of MainActivity to BottomVavigationActivity

--- app/src/main/java/com/example/hometeach/BottonNavigationActivity.kt
+++ app/src/main/java/com/example/hometeach/BottonNavigationActivity.kt
@@ -8,7 +8,7 @@ import com.example.hometeach.fragment.ParentStudentHomepageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class BottonNavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
