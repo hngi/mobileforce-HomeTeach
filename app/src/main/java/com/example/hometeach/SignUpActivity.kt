cfed13f35ec779c17679e12c812fbd95bf40d8cf
Sommie09 <ceekay2a7@gmail.com>
2020-06-20 16:37:25 +0100

    Sign Up Screen

---
+++ app/src/main/java/com/example/hometeach/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hometeach
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isEmpty
+import com.google.android.material.textfield.TextInputLayout
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        }
+}
