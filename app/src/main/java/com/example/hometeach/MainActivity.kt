0f7b15d409bbdfc1a095cc83c74a79cd067d2b3f
patrickishaf <peterndukwe96@gmail.com>
2020-06-21 10:37:51 +0100

    Built login layout

--- app/src/main/java/com/example/hometeach/MainActivity.kt
+++ app/src/main/java/com/example/hometeach/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.hometeach
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToLoginActivity(view: View) {
+        var loginActivityIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginActivityIntent)
     }
 }
