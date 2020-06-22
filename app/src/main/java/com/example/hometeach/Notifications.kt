e849535914c64593551c96c50dcdb84b009c8a1e
Officialboniface <officialboniface@gmail.com>
2020-06-21 19:26:30 -0600

    Notification fragment

---
+++ app/src/main/java/com/example/hometeach/Notifications.kt
@@ -0,0 +1,6 @@
+package com.example.hometeach
+
+data class Notifications(var  Title: String, var Time: Int, var Tag: String, var NotificationImage: Int) {
+
+}
+
---
+++ app/src/main/java/com/example/hometeach/adapter/NotificationAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.hometeach.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hometeach.Notifications
+import com.example.hometeach.R
+import kotlinx.android.synthetic.main.parent_notification_layout.view.*
+import java.util.*
+import java.util.logging.Filter
+import kotlin.collections.ArrayList
+
+class NotificationAdapter (var  items: ArrayList<Notifications>, var clickListener: OnNotificationItemClickListener) : RecyclerView.Adapter<NotificationsViewHolder>() {
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationsViewHolder {
+        lateinit var  notificationViewHolder: NotificationsViewHolder
+
+        notificationViewHolder = NotificationsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.fragment_parent_notification, parent, false))
+        return notificationViewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: NotificationsViewHolder, position: Int) {
+
+        holder.initialize(items.get(position), clickListener)
+
+    }
+
+}
+
+
+
+class  NotificationsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    var NotificationTitle = itemView.Notification_Title
+    var NotificationTime = itemView.Time_Tv
+    var NotificationImage = itemView.Circle_Image
+    var NotificationTag = itemView.Notification_Tag
+
+
+
+    fun initialize (item: Notifications, action: OnNotificationItemClickListener){
+        NotificationTitle.text = item.Title
+        NotificationTime.text = item.Time.toString()
+        NotificationTag.text = item.Tag
+        NotificationImage.setImageResource(item.NotificationImage)
+
+
+
+        itemView.setOnClickListener{
+            action.onItemClick(item, adapterPosition)
+        }
+
+    }
+
+
+
+}
+
+interface OnNotificationItemClickListener{
+    fun onItemClick(items: Notifications, position: Int)
+
+}
