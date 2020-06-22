bfa7bfb681bf0827a25d724e6fdfc534821eed5c
= <=>
2020-06-22 02:56:06 +0100

    created fragment for chat list

---
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
@@ -0,0 +1,26 @@
+package com.mobileforce.hometeach.ui.message
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobileforce.hometeach.R
+
+/**
+ * Authored by enyason
+ */
+
+class ChatListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_list, container, false)
+    }
+
+
+}
