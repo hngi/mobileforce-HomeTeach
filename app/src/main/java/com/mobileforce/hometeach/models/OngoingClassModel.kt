7d5790002671f7584f27f3447e05c2168a0db494
MUSA ABDUL-WADUD <wadva474@gmail.com>
2020-06-22 13:44:58 +0100

    Feat : OngoingFragmentTutor

--- app/src/main/java/com/mobileforce/hometeach/models/OngoingClassModel.kt
+++ app/src/main/java/com/mobileforce/hometeach/models/OngoingClassModel.kt
@@ -8,6 +8,12 @@ class OngoingClassModel(
     val tutorImage:String,
     val progress: Int,
     val tutorSubject: String) {
-
-
 }
+
+data class OngoingClassModelTutor(val subject: String,
+                                  val date: String,
+                                  val time: String,
+                                  val tutorName: String,
+                                  val tutorImage:String,
+                                  val progress: Int,
+                                  val mutualLesson: Int)
