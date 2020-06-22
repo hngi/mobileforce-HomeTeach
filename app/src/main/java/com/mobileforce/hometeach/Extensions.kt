e38aecd05d9799accb9bf0ece4f7ba968010ed90
= <=>
2020-06-22 15:16:07 +0100

    hooked up all components for chat list screen to communicate together

--- app/src/main/java/com/mobileforce/hometeach/Extensions.kt
+++ app/src/main/java/com/mobileforce/hometeach/Extensions.kt
@@ -1,10 +1,24 @@
 package com.mobileforce.hometeach
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.LayoutRes
 import androidx.constraintlayout.widget.Placeholder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
+/**
+ * Authored by enyason
+ */
+
+/**
+ * load image into an image view
+ * @param image can be a url,a drawable, an file resource id etc
+ * @param placeholder is the default image that shows when actual image is being loaded or in case of an error
+ * @param circular determines if a circular transformation is used to achieve a circular image view
+ */
 fun ImageView.loadImage(image: Any, placeholder: Int = 0, circular: Boolean = false) {
     Glide
         .with(context)
@@ -18,4 +32,11 @@ fun ImageView.loadImage(image: Any, placeholder: Int = 0, circular: Boolean = fa
             }
         )
         .into(this)
+}
+
+/**
+ * inflate layout using layout inflater with context from the parent
+ */
+fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layout, this, attachToRoot)
 }
--- app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
@@ -2,24 +2,57 @@ package com.mobileforce.hometeach.ui.message
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobileforce.hometeach.R
+import com.mobileforce.hometeach.models.Chat
+import com.mobileforce.hometeach.models.chatDiffUtil
+import com.mobileforce.hometeach.adapters.RecyclerViewAdapter
+import com.mobileforce.hometeach.adapters.ViewHolder
+import kotlinx.android.synthetic.main.fragment_chat_list.*
 
 /**
  * Authored by enyason
  */
 
-class ChatListFragment : Fragment() {
+class ChatListFragment :
+    Fragment(R.layout.fragment_chat_list)/* the constructor takes the layout which will be inflated at onCreateView*/ {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_list, container, false)
+    private val chatListItemClickListener: (String) -> Unit = { chatId ->
+
+        //navigate to message screen
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /**
+         * build chat list adapter
+         */
+        val chatListAdpter = object : RecyclerViewAdapter<Chat>(chatDiffUtil) {
+            override fun getLayoutRes(): Int {
+                return R.layout.chat_list_message_item
+            }
+
+            override fun getViewHolder(
+                view: View,
+                recyclerViewAdapter: RecyclerViewAdapter<Chat>
+            ): ViewHolder<Chat> {
+                return ChatListViewHolder(view, chatListItemClickListener)
+            }
+
+        }
+
+        //setup chat list recycler view
+        chatListRecyclerView.apply {
+            adapter = chatListAdpter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+
+
+
     }
 
 
