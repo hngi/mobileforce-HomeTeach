5b51d2f4875237973afbfbbb0cf5dd9beace62ff
dolapobabs <user@dolapobabs-2.local>
2020-06-23 03:25:26 +0100

    Feat/TutorList

---
+++ app/src/main/java/com/mobileforce/hometeach/models/TutorAllModel.kt
@@ -0,0 +1,13 @@
+package com.mobileforce.hometeach.models
+
+data class TutorAllModel(
+
+    val tutorName: String,
+    val tutorImage:String,
+    val description: String,
+    val tutorSubject: String,
+    val perhouramount:Int,
+    var rating: String) {
+
+
+}
