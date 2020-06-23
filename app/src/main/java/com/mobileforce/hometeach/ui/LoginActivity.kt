95a23dc46c5293fac829d24e8e41d8918491945f
= <=>
2020-06-23 12:05:39 +0100

    change button id

--- app/src/main/java/com/mobileforce/hometeach/ui/LoginActivity.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.mobileforce.hometeach.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mobileforce.hometeach.R
+import com.mobileforce.hometeach.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signIn.setOnClickListener {
+            navigateToDashBoard()
+        }
+
+    }
+
+    private fun navigateToDashBoard() {
+        startActivity(Intent(this, BottonNavigationActivity::class.java))
     }
 }
