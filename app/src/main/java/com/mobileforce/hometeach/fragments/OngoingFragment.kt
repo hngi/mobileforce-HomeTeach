b01b723ce2fe37a6f8b2f30714b96df35d9bdc3c
= <=>
2020-06-23 02:33:01 +0100

    imported design icons for menu navigation bar

--- app/src/main/java/com/mobileforce/hometeach/fragments/OngoingFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/fragments/OngoingFragment.kt
@@ -21,22 +21,53 @@ import kotlinx.android.synthetic.main.list_item_class.*
  */
 class OngoingFragment : Fragment() {
     private lateinit var listView: ListView
-    private lateinit var ongoing_classes_list:MutableList<OngoingClassModel>
+    private var ongoing_classes_list = mutableListOf<OngoingClassModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_ongoing, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // RecyclerView node initialized here
-        ongoing_classes_list.add(OngoingClassModel("Physics - Insulators - Module 1","Tue, 10 July","16:00 - 18:00","Rahman Django","profile_image",40,"Physics Tutor"))
-        ongoing_classes_list.add(OngoingClassModel("Biology - Module 2","Tue, 10 July","16:00 - 18:00","Alice Snow","profile_image",100,"Biology Tutor"))
-        ongoing_classes_list.add(OngoingClassModel("Mathematics - Module 1","Tue, 10 July","16:00 - 18:00","Alice Snow","https://via.placeholder.com/300/09f/fff.png",100,"Mathematics Tutor"))
+        ongoing_classes_list.add(
+            OngoingClassModel(
+                "Physics - Insulators - Module 1",
+                "Tue, 10 July",
+                "16:00 - 18:00",
+                "Rahman Django",
+                "profile_image",
+                40,
+                "Physics Tutor"
+            )
+        )
+        ongoing_classes_list.add(
+            OngoingClassModel(
+                "Biology - Module 2",
+                "Tue, 10 July",
+                "16:00 - 18:00",
+                "Alice Snow",
+                "profile_image",
+                100,
+                "Biology Tutor"
+            )
+        )
+        ongoing_classes_list.add(
+            OngoingClassModel(
+                "Mathematics - Module 1",
+                "Tue, 10 July",
+                "16:00 - 18:00",
+                "Alice Snow",
+                "https://via.placeholder.com/300/09f/fff.png",
+                100,
+                "Mathematics Tutor"
+            )
+        )
 //        Log.d("count",ongoing_classes_list.size.toString())
         classes_recycler_view.apply {
-            layoutManager= LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
             adapter = OngoingRecyclerAdapter(ongoing_classes_list)
 
         }
@@ -44,6 +75,7 @@ class OngoingFragment : Fragment() {
 
 
     }
+
     companion object {
         fun newInstance(): OngoingFragment =
             OngoingFragment()
--- app/src/main/java/com/mobileforce/hometeach/ui/BottonNavigationActivity.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/BottonNavigationActivity.kt
@@ -3,53 +3,63 @@ package com.mobileforce.hometeach.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mobileforce.hometeach.fragment.ParentStudentHomepageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mobileforce.hometeach.R
+import com.mobileforce.hometeach.databinding.BottonNavLayoutBinding
 
 //import kotlinx.android.synthetic.main.activity_main.*
 
-class BottonNavigationActivity : AppCompatActivity() {
+class BottonNavigationActivity : AppCompatActivity() {;
+
+    lateinit var binding: BottonNavLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.botton_nav_layout)
+        binding = BottonNavLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navController = findNavController(this, R.id.nav_host_fragment)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
 
         //  bottomNavigationView.setOnNavigationItemReselectedListener(mOnnavigationitemselected)
-        val bottomNavigationView= findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-
-            when (it.itemId) {
-
-                R.id.books -> {
-                  //call the load fragment method and passs the name of the fragment
-
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.contacts -> {
-                    //call the load fragment method and passs the name of the fragment
-                    return@setOnNavigationItemSelectedListener true
-                }
-
-
-                R.id.home -> {
-                    loadFragment(ParentStudentHomepageFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.notification -> {
-                    //call the load fragment method and passs the name of the fragment
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.profile -> {
-                    //call the load fragment method and passs the name of the fragment
-                    return@setOnNavigationItemSelectedListener true
-                }
-
-            }
-            false
-        }
-
-        }
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        bottomNavigationView.setOnNavigationItemSelectedListener {
+//
+//            when (it.itemId) {
+//
+//                R.id.books -> {
+//                    //call the load fragment method and passs the name of the fragment
+//
+//                    return@setOnNavigationItemSelectedListener true
+//                }
+//                R.id.contacts -> {
+//                    //call the load fragment method and passs the name of the fragment
+//                    return@setOnNavigationItemSelectedListener true
+//                }
+//
+//
+//                R.id.home -> {
+////                    loadFragment(ParentStudentHomepageFragment())
+//                    return@setOnNavigationItemSelectedListener true
+//                }
+//                R.id.notification -> {
+//                    //call the load fragment method and passs the name of the fragment
+//                    return@setOnNavigationItemSelectedListener true
+//                }
+//                R.id.profile -> {
+//                    //call the load fragment method and passs the name of the fragment
+//                    return@setOnNavigationItemSelectedListener true
+//                }
+//
+//            }
+//            false
+//        }
+//
+//    }
 
 //        bottomNavigationView.setOnNavigationItemSelectedListener {
 //
@@ -84,12 +94,12 @@ class BottonNavigationActivity : AppCompatActivity() {
 //        }
 //
 //
-//    }
+    }
 
     private fun loadFragment(fragment: Fragment) {
         // load fragment
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_layout, fragment)
+//        transaction.replace(R.id.frame_layout, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
     }
