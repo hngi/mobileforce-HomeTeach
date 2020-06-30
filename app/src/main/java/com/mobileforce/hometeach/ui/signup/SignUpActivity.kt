c8694b176550ab43b7b879c6ed7a8a4d351bb433
peculiaruc <peculiarumeh02@gmail.com>
2020-06-30 13:03:03 +0100

    feat: sign up screen

---
+++ app/src/main/java/com/mobileforce/hometeach/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.mobileforce.hometeach.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobileforce.hometeach.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
