fa4055a0fc285a08a69cb6ef13474d394d5ee2e4
mayorjay <joseolu4gsm@yahoo.com>
2020-07-03 03:48:46 +0100

    Feat: Create Chat Fragment, necessary layouts and drawable resources

---
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatFragment.kt
@@ -0,0 +1,32 @@
+package com.mobileforce.hometeach.ui.message
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobileforce.hometeach.R
+
+class ChatFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ChatFragment()
+    }
+
+    private lateinit var viewModel: ChatViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.chat_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ChatViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatViewModel.kt
@@ -0,0 +1,7 @@
+package com.mobileforce.hometeach.ui.message
+
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
