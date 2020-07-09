51df9e628b3cf6fd65e3217e70aa8e3e724e158d
unknown <gbubemimakpokpomi@gmail.com>
2020-07-09 03:13:53 +0100

    added reset-password with email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('register/', views.api_register_view),
     path('activate/<slug:uidb64>/<slug:token>', views.activate, name='activate'),
+
+    # The django-rest-passwordreset urls to request a token and confirm pw-reset
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    # password_reset/confirm/
+    # password_reset/validate_token/
 ]
