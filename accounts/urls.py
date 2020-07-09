df8accc25bd052258a136136152d4f386025e86c
Steven Kolawole <kolawolesteven99@gmail.com>
2020-07-09 19:14:29 +0100

    Expose the Logout API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
-from .views import UserLoginView
 
 urlpatterns = [
     path('register/', views.api_register_view),
-    path('login/', UserLoginView.as_view(), name = 'login'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('activate/<slug:uidb64>/<slug:token>', views.activate, name='activate'),
 
     # The django-rest-passwordreset urls to request a token and confirm pw-reset
