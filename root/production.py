5106f829d1943ce7530df76a28593f74320d9273
believemanasseh <believemanasseh@gmail.com>
2020-07-10 00:39:33 +0100

    heroku deploy

--- root/production.py
+++ root/production.py
@@ -71,6 +71,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = { 'default': dj_database_url.config() }
 
+
 ROOT_URLCONF = 'root.urls'
 
 TEMPLATES = [
