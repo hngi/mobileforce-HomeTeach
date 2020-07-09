4808fb401c3a8cac631a8b3275548ae74599daed
mikail <mikailkyusuf@gmail.com>
2020-07-09 08:50:36 +0100

    Implemented Recover Password Dialog

--- app/src/main/java/com/mobileforce/hometeach/ui/LoginActivity.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/LoginActivity.kt
@@ -3,9 +3,16 @@ package com.mobileforce.hometeach.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import com.mobileforce.hometeach.R
 import com.mobileforce.hometeach.databinding.ActivityLoginBinding
 import com.mobileforce.hometeach.ui.signup.SignUpActivity
+import kotlinx.android.synthetic.main.forgot_password_layout1.view.*
+import kotlinx.android.synthetic.main.forgot_password_layout1.view.apply
+import kotlinx.android.synthetic.main.recover_email_layout.view.*
+import kotlinx.android.synthetic.main.recover_phone_layout.view.*
+import kotlin.apply
 
 class LoginActivity : AppCompatActivity() {
 
@@ -22,6 +29,9 @@ class LoginActivity : AppCompatActivity() {
         binding.registerNowButton.setOnClickListener {
             navigateToSignUp()
         }
+        binding.forgotPassword.setOnClickListener {
+            forgotPassword()
+        }
 
     }
 
@@ -32,4 +42,50 @@ class LoginActivity : AppCompatActivity() {
     private fun navigateToSignUp() {
         startActivity(Intent(this, SignUpActivity::class.java))
     }
+
+    private fun forgotPassword() {
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.forgot_password_layout1, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+
+        mDialogView.apply.setOnClickListener {
+            mAlertDialog.dismiss()
+
+            if (mDialogView.recover_email_chBox.isChecked) {
+                showEmailDialog()
+            } else if (mDialogView.recover_phone_chBox.isChecked) {
+                showPhoneDialog()
+            } else {
+
+            }
+
+
+        }
+
+    }
+
+    private fun showEmailDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.recover_email_layout, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+        val email = mDialogView.emailtext.text.toString()
+    }
+
+    private fun showPhoneDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.recover_phone_layout, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+        val phone = mDialogView.phone.text.toString()
+    }
+
 }
