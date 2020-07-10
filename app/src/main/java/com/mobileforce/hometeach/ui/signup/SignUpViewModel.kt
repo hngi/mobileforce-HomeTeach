765b72f80dcebb8c906b856f277937ac835d4e38
enyason <enyasonjnr@gmail.com>
2020-07-10 21:44:16 +0100

    init view model for sign up

---
+++ app/src/main/java/com/mobileforce/hometeach/ui/signup/SignUpViewModel.kt
@@ -0,0 +1,9 @@
+package com.mobileforce.hometeach.ui.signup
+
+import androidx.lifecycle.ViewModel
+import com.mobileforce.hometeach.data.repo.UserRepository
+
+class SignUpViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+
+}
