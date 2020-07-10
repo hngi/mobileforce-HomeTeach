8ac944c974b2364431641e119a028ff7566e97c1
enyason <enyasonjnr@gmail.com>
2020-07-10 23:28:01 +0100

    make success data nullable, to account for success scenario where response is not required

--- app/src/main/java/com/mobileforce/hometeach/utils/Result.kt
+++ app/src/main/java/com/mobileforce/hometeach/utils/Result.kt
@@ -11,7 +11,7 @@ package com.mobileforce.hometeach.utils
  */
 sealed class Result<out R> {
 
-    data class Success<out T>(val data: T) : Result<T>()
+    data class Success<out T>(val data: T? = null) : Result<T>()
     data class Error(val exception: Throwable) : Result<Nothing>()
     object Loading : Result<Nothing>()
 
