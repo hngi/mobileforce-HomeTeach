7b2e2a52ca72b8c4ae602089b4ae2f0276c4f395
enyason <enyasonjnr@gmail.com>
2020-07-10 21:43:56 +0100

    set up data class as request body for register endpoint

--- app/src/main/java/com/mobileforce/hometeach/remotesource/Params.kt
+++ app/src/main/java/com/mobileforce/hometeach/remotesource/Params.kt
@@ -1,9 +1,21 @@
 package com.mobileforce.hometeach.remotesource
 
+/**
+ * Authored by enyason
+ */
+
 class Params {
 
     data class SignIn(
         val email: String,
         val password: String
+    )
+
+    data class SignUp(
+        val email: String,
+        val password: String,
+        val fullName: String,
+        val isTutor: Boolean,
+        val number: String
     )
 }
