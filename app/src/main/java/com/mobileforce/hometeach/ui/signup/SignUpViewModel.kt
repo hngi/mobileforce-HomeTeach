c44c51f553b3a9b50ceaa492c2780e21de7d7925
enyason <enyasonjnr@gmail.com>
2020-07-10 23:08:13 +0100

    add live data to sign up view model, to hold api response

--- app/src/main/java/com/mobileforce/hometeach/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/signup/SignUpViewModel.kt
@@ -1,14 +1,26 @@
 package com.mobileforce.hometeach.ui.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mobileforce.hometeach.data.repo.UserRepository
 import com.mobileforce.hometeach.remotesource.Params
+import com.mobileforce.hometeach.utils.Result
+import kotlinx.coroutines.launch
 
 class SignUpViewModel(private val userRepository: UserRepository) : ViewModel() {
 
+    private val _signUp = MutableLiveData<Result<Nothing>>()
+    val signUp: LiveData<Result<Nothing>>
+        get() = _signUp
 
     fun signUp(params: Params.SignUp) {
+        _signUp.postValue(Result.Loading)
+        viewModelScope.launch {
 
+
+        }
 
     }
 }
