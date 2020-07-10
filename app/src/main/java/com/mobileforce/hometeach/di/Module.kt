dd002f8d856b56ee04fb946010cd9f45666ac196
enyason <enyasonjnr@gmail.com>
2020-07-10 22:59:09 +0100

    added http client logger interceptor to log api request and response

--- app/src/main/java/com/mobileforce/hometeach/di/Module.kt
+++ app/src/main/java/com/mobileforce/hometeach/di/Module.kt
@@ -5,7 +5,6 @@ import com.mobileforce.hometeach.AppConstants.BASE_URL
 import com.mobileforce.hometeach.AppConstants.DATABASE_NAME
 import com.mobileforce.hometeach.data.UserRepositoryImpl
 import com.mobileforce.hometeach.data.repo.UserRepository
-import com.mobileforce.hometeach.data.sources.DataSource
 import com.mobileforce.hometeach.data.sources.DataSourceFactory
 import com.mobileforce.hometeach.data.sources.LocalDataSource
 import com.mobileforce.hometeach.data.sources.RemoteDataSource
@@ -13,10 +12,13 @@ import com.mobileforce.hometeach.localsource.AppDataBase
 import com.mobileforce.hometeach.localsource.PreferenceHelper
 import com.mobileforce.hometeach.remotesource.Api
 import com.mobileforce.hometeach.ui.signin.SignInViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 val appModule = module {
@@ -31,9 +33,24 @@ val appModule = module {
     single<Api> {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            //TODO add http logger
+            .client(get())
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(Api::class.java)
+    }
+
+    single<OkHttpClient> {
+
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .connectTimeout(30, TimeUnit.SECONDS) //Backend is really slow
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        httpClient
     }
 
 
