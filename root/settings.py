6fb16c9f0ea9ec4adc96e6440d9ff91d61841d4c
believemanasseh <believemanasseh@gmail.com>
2020-07-12 01:00:13 +0100

    modified dependencies

--- root/settings.py
+++ root/settings.py
@@ -148,7 +148,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #location where django collect all static files
@@ -157,10 +156,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'hometeach/static')
 ]
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
