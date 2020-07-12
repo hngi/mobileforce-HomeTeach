ead270a47a92be29d56b47bc6c832a00895dc9f7
believemanasseh <believemanasseh@gmail.com>
2020-07-13 00:43:10 +0100

    ran migrations

--- accounts/views.py
+++ accounts/views.py
@@ -76,7 +76,7 @@ def activate(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
