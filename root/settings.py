b418c4885dd858d3b42c77de5d289d77b4b21d0b
believemanasseh <believemanasseh@gmail.com>
2020-07-12 01:30:14 +0100

    modified static root

--- root/settings.py
+++ root/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # location where you will store your static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
