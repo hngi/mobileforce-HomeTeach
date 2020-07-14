439c13bfc2e1cb6068af070e6c4db8b992adadf7
dolapobabs <dolapob@gmail.com>
2020-07-14 10:49:50 +0100

    added tutorrepository

--- app/src/main/java/com/mobileforce/hometeach/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/profile/ProfileViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mobileforce.hometeach.data.repo.TutorRepository
 import com.mobileforce.hometeach.data.repo.UserRepository
 import com.mobileforce.hometeach.remotesource.Params
 import com.mobileforce.hometeach.remotesource.wrappers.ProfileResponse
@@ -11,7 +12,7 @@ import com.mobileforce.hometeach.remotesource.wrappers.TutorDetailsResponse
 import com.mobileforce.hometeach.utils.Result
 import kotlinx.coroutines.launch
 
-class ProfileViewModel(private val userRepository: UserRepository) : ViewModel()  {
+class ProfileViewModel(private val tutorRepository: TutorRepository) : ViewModel()  {
 
     private val _getTutorDetails = MutableLiveData<Result<TutorDetailsResponse>>()
     val getTutorDetails: LiveData<Result<TutorDetailsResponse>> = _getTutorDetails
@@ -21,7 +22,7 @@ class ProfileViewModel(private val userRepository: UserRepository) : ViewModel()
         _getTutorDetails.postValue(Result.Loading)
         viewModelScope.launch {
             try {
-                val response = userRepository.getTutorDetails()
+                val response = tutorRepository.getTutorDetails()
                 _getTutorDetails.postValue(Result.Success(response))
             } catch (error: Throwable) {
                 _getTutorDetails.postValue(Result.Error(error))
