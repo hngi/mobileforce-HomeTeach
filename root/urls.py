fafd796b03893fe5e30a398c0ceb3fa611527e00
MdBilal420 <mohdbilal16@gmail.com>
2020-07-15 13:05:47 +0530

    Course Registration

--- root/urls.py
+++ root/urls.py
@@ -34,6 +34,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
+    path('api/v1/', include('confirmation.urls')),
     path('api/v1/', include('api.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
