27aa59514c119bf81b925b1fb04de146b5a1012f
believemanasseh <believemanasseh@gmail.com>
2020-07-17 04:10:53 +0100

    minor changes

--- paystack/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class TransactionsSerializer(serializers.Serializer):
-	email = serializers.EmailField(allow_blank=False)
-	amount = serializers.IntegerField(max_value=None, min_value=None)
-	#ref = serializers.UUIDField(format='hex')
-
