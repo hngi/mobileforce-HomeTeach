0e8aa2a724f5487d565bebe91b9d14e8a82781cd
shols232 <akinsolaademolatemitope@gmail.com>
2020-07-17 10:00:22 +0100

    commit

--- root/settings.py
+++ root/settings.py
@@ -14,11 +14,15 @@ import os
 import django_heroku
 import dj_database_url
 from dotenv import load_dotenv
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -100,12 +104,8 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 #db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES = { 'default': dj_database_url.config() }
@@ -207,4 +207,4 @@ PAYSTACK_AUTHORIZATION_KEY = 'sk_test_72d039a582a3504fdeeffd3930914247ba070db3'
 
 
 # ENCRYPT_KEY = b'5R_y8WWIMF7MOhShxQiZFZwXcRGGKKbdGrkPN9iVVpc='
-
+del DATABASES['default']['OPTIONS']['sslmode']
