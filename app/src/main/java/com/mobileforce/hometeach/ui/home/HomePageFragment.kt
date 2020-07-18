17a8b4320a52dab0f4d63420a69314edf065fa4e
pecpaker <umehpeculiar20@gmail.com>
2020-07-18 10:04:40 +0100

    feat/tutorhomepage/dashboard

--- app/src/main/java/com/mobileforce/hometeach/ui/home/HomePageFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/home/HomePageFragment.kt
@@ -20,6 +20,7 @@ import androidx.core.view.ViewCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
@@ -40,6 +41,7 @@ import com.mobileforce.hometeach.utils.AppConstants.USER_TUTOR
 import kotlinx.android.synthetic.main.fragment_home_page_tutor.*
 
 import com.mobileforce.hometeach.utils.PreferenceHelper
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import java.text.SimpleDateFormat
@@ -411,8 +413,7 @@ class HomePageFragment : Fragment() {
         viewModel.user.observe(viewLifecycleOwner, androidx.lifecycle.Observer { user ->
 
 
-           
-            user?.let {
+        user?.let {
                 bindingTutor.username.text = "Welcome ${user.full_name}"
             }
         })
