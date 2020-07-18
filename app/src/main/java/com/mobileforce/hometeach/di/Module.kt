78ca1a2ed191afe681df51a032147804071dff24
enyason <enyasonjnr@gmail.com>
2020-07-18 11:55:33 +0100

    fix merge issue

--- app/src/main/java/com/mobileforce/hometeach/di/Module.kt
+++ app/src/main/java/com/mobileforce/hometeach/di/Module.kt
@@ -1,28 +1,28 @@
 package com.mobileforce.hometeach.di
 
 import androidx.room.Room
-import com.mobileforce.hometeach.utils.AppConstants.BASE_URL
-import com.mobileforce.hometeach.utils.AppConstants.DATABASE_NAME
-import com.mobileforce.hometeach.data.UserRepositoryImpl
-import com.mobileforce.hometeach.data.repository.UserRepository
 import com.mobileforce.hometeach.data.TutorRepositoryImpl
+import com.mobileforce.hometeach.data.UserRepositoryImpl
 import com.mobileforce.hometeach.data.repository.TutorRepository
+import com.mobileforce.hometeach.data.repository.UserRepository
 import com.mobileforce.hometeach.data.sources.DataSourceFactory
 import com.mobileforce.hometeach.data.sources.LocalDataSource
 import com.mobileforce.hometeach.data.sources.RemoteDataSource
 import com.mobileforce.hometeach.data.sources.local.AppDataBase
-import com.mobileforce.hometeach.utils.PreferenceHelper
 import com.mobileforce.hometeach.data.sources.remote.Api
-
 import com.mobileforce.hometeach.ui.home.HomePageViewModel
+import com.mobileforce.hometeach.ui.message.ChatViewModel
 import com.mobileforce.hometeach.ui.profile.EditTutorViewModel
 import com.mobileforce.hometeach.ui.profile.ProfileViewModel
 import com.mobileforce.hometeach.ui.signin.SignInViewModel
 import com.mobileforce.hometeach.ui.signup.SignUpViewModel
-import com.mobileforce.hometeach.ui.tutorlist.TutorListViewModel
 import com.mobileforce.hometeach.ui.studentpayments.carddetails.StudentAddCardDetailsViewModel
 import com.mobileforce.hometeach.ui.studentpayments.carddetails.StudentCardDetailsViewModel
 import com.mobileforce.hometeach.ui.studentpayments.makepayments.StudentMakePaymentViewModel
+import com.mobileforce.hometeach.ui.tutorlist.TutorListViewModel
+import com.mobileforce.hometeach.utils.AppConstants.BASE_URL
+import com.mobileforce.hometeach.utils.AppConstants.DATABASE_NAME
+import com.mobileforce.hometeach.utils.PreferenceHelper
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
