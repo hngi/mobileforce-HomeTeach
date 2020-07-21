fe6a21b7728bc36be66018d445dbe2f1dd18c483
enyason <enyasonjnr@gmail.com>
2020-07-21 08:19:10 +0100

    fetch chat list from view model

--- app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.mobileforce.hometeach.R
 import com.mobileforce.hometeach.adapters.RecyclerViewAdapter
 import com.mobileforce.hometeach.adapters.ViewHolder
@@ -24,7 +22,6 @@ class ChatListFragment :
     Fragment(R.layout.fragment_chat_list)/* the constructor takes the layout which will be inflated at onCreateView*/ {
 
 
-    private val db = Firebase.firestore
     private val viewModel: ChatViewModel by sharedViewModel()
 
     private val chatListItemClickListener: (Chat) -> Unit = { chat ->
@@ -61,55 +58,18 @@ class ChatListFragment :
             layoutManager = LinearLayoutManager(requireContext())
         }
 
+        viewModel.fetchUserChatList()
 
         viewModel.user.observe(viewLifecycleOwner, Observer { user ->
-
-            db.clearPersistence()
             user?.let {
-                db
-                    .collection("user")
-                    .document(it.id)
-                    .collection("connect")
-                    .addSnapshotListener { snapshot, error ->
 
-                        error?.let {
-                            return@addSnapshotListener
-                        }
+            }
+        })
 
-                        if (snapshot != null) {
+        viewModel.chatList.observe(viewLifecycleOwner, Observer {
 
-                            val chatList = mutableListOf<Chat>()
-                            for (doc in snapshot.documents) {
-
-                                val name = doc.getString("full_name")
-                                val senderId = doc.getString("id")
-                                val connectId = doc.getString("connect_id")
-                                val lastMessage = doc.getString("last_message")
-
-
-                                val chat = Chat(
-                                    chatId = connectId.toString(),
-                                    senderName = name.toString(),
-                                    senderPhoto = "",
-                                    lastMessage = lastMessage.toString(),
-                                    lastMessageTime = "Today",
-                                    senderId = senderId!!
-
-                                )
-
-                                chatList.add(chat)
-
-                            }
-
-                            chatListAdpter.submitList(chatList)
-
-
-                        } else {
-                            println("Current data: null")
-                        }
-
-
-                    }
+            it?.let { list ->
+                chatListAdpter.submitList(list)
             }
 
         })
--- app/src/main/java/com/mobileforce/hometeach/ui/message/ChatViewModel.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatViewModel.kt
@@ -1,12 +1,70 @@
 package com.mobileforce.hometeach.ui.message
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.mobileforce.hometeach.data.repository.UserRepository
 import com.mobileforce.hometeach.models.Chat
 
 class ChatViewModel(private val userRepository: UserRepository) : ViewModel() {
 
+    private val db = Firebase.firestore
+
     val user = userRepository.getUser()
     var chatListItem: Chat? = null
+
+    private val _chatList = MutableLiveData<List<Chat>>()
+    val chatList: LiveData<List<Chat>>
+        get() = _chatList
+
+
+    fun fetchUserChatList() {
+
+        db
+            .collection("user")
+            .document(user.value!!.id)
+            .collection("connect")
+            .addSnapshotListener { snapshot, error ->
+
+                error?.let {
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null) {
+
+                    val chatList = mutableListOf<Chat>()
+                    for (doc in snapshot.documents) {
+
+                        val name = doc.getString("full_name")
+                        val senderId = doc.getString("id")
+                        val connectId = doc.getString("connect_id")
+                        val lastMessage = doc.getString("last_message")
+
+
+                        val chat = Chat(
+                            chatId = connectId.toString(),
+                            senderName = name.toString(),
+                            senderPhoto = "",
+                            lastMessage = lastMessage.toString(),
+                            lastMessageTime = "Today",
+                            senderId = senderId!!
+                        )
+
+                        chatList.add(chat)
+
+                    }
+
+                    _chatList.postValue(chatList)
+
+                } else {
+                    println("Current data: null")
+                }
+
+
+            }
+
+    }
 
 }
