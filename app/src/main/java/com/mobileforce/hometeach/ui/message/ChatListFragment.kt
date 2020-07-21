465c1312486bcd834914995541477fe82a440912
enyason <enyasonjnr@gmail.com>
2020-07-21 08:28:23 +0100

    * refactor chat list to use view binding * implemented search filter on the chat list

--- app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/message/ChatListFragment.kt
@@ -1,7 +1,11 @@
 package com.mobileforce.hometeach.ui.message
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -9,20 +13,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobileforce.hometeach.R
 import com.mobileforce.hometeach.adapters.RecyclerViewAdapter
 import com.mobileforce.hometeach.adapters.ViewHolder
+import com.mobileforce.hometeach.databinding.FragmentChatListBinding
 import com.mobileforce.hometeach.models.Chat
 import com.mobileforce.hometeach.models.chatDiffUtil
-import kotlinx.android.synthetic.main.fragment_chat_list.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 /**
  * Authored by enyason
  */
 
-class ChatListFragment :
-    Fragment(R.layout.fragment_chat_list)/* the constructor takes the layout which will be inflated at onCreateView*/ {
+class ChatListFragment : Fragment() {
 
 
     private val viewModel: ChatViewModel by sharedViewModel()
+
+    private lateinit var binding: FragmentChatListBinding
 
     private val chatListItemClickListener: (Chat) -> Unit = { chat ->
 
@@ -31,6 +36,14 @@ class ChatListFragment :
         findNavController().navigate(R.id.action_chatListFragment_to_chatFragment)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatListBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -53,7 +66,7 @@ class ChatListFragment :
         }
 
         //setup chat list recycler view
-        chatListRecyclerView.apply {
+        binding.chatListRecyclerView.apply {
             adapter = chatListAdpter
             layoutManager = LinearLayoutManager(requireContext())
         }
@@ -70,6 +83,38 @@ class ChatListFragment :
 
             it?.let { list ->
                 chatListAdpter.submitList(list)
+            }
+
+        })
+
+
+        // handle in app filter
+        binding.chatListSearchView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (!s.isNullOrEmpty()) {
+
+                    val filter = viewModel.chatList.value?.filter {
+                        s.toString() == it.senderName
+                    }
+
+                    filter?.let {
+                        chatListAdpter.submitList(it)
+                    }
+                } else {
+
+                    viewModel.chatList.value?.let {
+                        chatListAdpter.submitList(it)
+                    }
+                }
             }
 
         })
