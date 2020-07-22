2e7f1429b4883731686ae3befb3e4bdfdf36b34c
mayorjay <joseolu4gsm@yahoo.com>
2020-07-22 13:14:46 +0100

    Fix: Onboarding and sign in screen corrections

--- app/src/main/java/com/mobileforce/hometeach/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/profile/ProfileFragment.kt
@@ -60,27 +60,18 @@ class ProfileFragment : Fragment() {
             bindingTutor.toolBar.setNavigationOnClickListener {
                 findNavController().popBackStack()
             }
-
-
         }else{
             setUpProfileForUser()
             bindingStudent.toolBar.setNavigationOnClickListener {
                 findNavController().popBackStack()
             }
         }
-
-
-
-        bindingTutor.editButton.setOnClickListener {
-
-            val bundle = bundleOf("imageUrl" to imageUrl,"tutorName" to tutorName)
-            navController.navigate(R.id.editTutorProfileFragment,bundle)
-        }
-
     }
+
     private fun setUpProfileForTutor(){
         bindingTutor.editButton.setOnClickListener {
-            findNavController().navigate(R.id.action_profileFragment_to_editTutorProfileFragment)
+            val bundle = bundleOf("imageUrl" to imageUrl,"tutorName" to tutorName)
+            navController.navigate(R.id.editTutorProfileFragment, bundle)
         }
         viewModel.getTutorDetails()
         viewModel.getTutorDetails.observe(viewLifecycleOwner, Observer { result ->
