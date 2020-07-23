72feb2db7da698171ecbfbc03a6bf89a50f7b94e
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-07-23 11:46:43 +0100

    Feat: change request tutor service flow

---
+++ app/src/main/java/com/mobileforce/hometeach/ui/tutordetails/TutorDetailsFragment.kt
@@ -0,0 +1,62 @@
+package com.mobileforce.hometeach.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+import com.mobileforce.hometeach.R
+import com.mobileforce.hometeach.databinding.FragmentTutorDetailsBinding
+import com.mobileforce.hometeach.ui.tutorlist.DialogData
+import com.mobileforce.hometeach.ui.tutorlist.SelectDateDialog
+import kotlinx.android.synthetic.main.request_layout.view.*
+
+
+class TutorDetailsFragment : Fragment(), SelectDateDialog.SelectDateListener{
+    private lateinit var binding:FragmentTutorDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTutorDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.requestService.setOnClickListener {
+            val selectDateDialog = SelectDateDialog()
+            selectDateDialog.showPopupWindow(requireView(),this)
+        }
+
+        binding.viewAll.setOnClickListener {
+            val mDialogView = LayoutInflater.from(activity).inflate(R.layout.credentials_dialog, null)
+            val mBuilder = activity?.let { it1 ->
+                AlertDialog.Builder(it1)
+                    .setView(mDialogView)
+
+            }
+
+            val  mAlertDialog = mBuilder?.show()
+          mBuilder?.setTitle("Credentials")
+            mDialogView.btn.setOnClickListener {
+                mAlertDialog?.hide()
+            }
+        }
+
+
+    }
+
+    override fun onApproveClicked(dialogData: DialogData) {
+        
+    }
+
+
+}
