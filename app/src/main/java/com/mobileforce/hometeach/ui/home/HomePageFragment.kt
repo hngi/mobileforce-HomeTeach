01969dc96f8c3c6449730161e8f65f780d9546f2
enyason <enyasonjnr@gmail.com>
2020-07-23 12:48:44 +0100

    fix merge issue

--- app/src/main/java/com/mobileforce/hometeach/ui/home/HomePageFragment.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/home/HomePageFragment.kt
@@ -7,10 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
