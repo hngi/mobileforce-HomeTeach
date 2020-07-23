99709896333f178650e80a1a774d46bce168467d
Prince <chimeremezechukwudire@gmail.com>
2020-07-23 21:55:02 +0100

    find tutor section added

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,23 +1,10 @@
-// 
-
-// external js: flickity.pkgd.js
 var $carousel = $('.carousel').flickity({
     imagesLoaded: true,
     percentPosition: false,
-});
+    freeScroll: true,
+    contain: true,
+    autoPlay: true,
+    pauseAutoPlayOnHover: true,
 
-var $imgs = $carousel.find('.carousel-cell img');
-// get transform property
-var docStyle = document.documentElement.style;
-var transformProp = typeof docStyle.transform == 'string' ?
-    'transform' : 'WebkitTransform';
-// get Flickity instance
-var flkty = $carousel.data('flickity');
 
-$carousel.on('scroll.flickity', function() {
-    flkty.slides.forEach(function(slide, i) {
-        var img = $imgs[i];
-        var x = (slide.target + flkty.x) * -1 / 3;
-        img.style[transformProp] = 'translateX(' + x + 'px)';
-    });
 });
