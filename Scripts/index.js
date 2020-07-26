eed506d7dc2ca36cc7e06821f59bddfcbb78da7a
Prince <chimeremezechukwudire@gmail.com>
2020-07-26 13:31:06 +0100

    fe-develop branch

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,8 @@
 // Initialize AOS HERE
 AOS.init();
 
+// Tyed js Here
+
 $('document').ready(function() {
     var typed = new Typed('#typed', {
         strings: ['Anytime... ^1000', 'Anywhere...'],
@@ -12,6 +14,8 @@ $('document').ready(function() {
         loop: true,
     });
 })
+
+// Carouse script here
 
 var $carousel = $('.carousel').flickity({
     imagesLoaded: true,
@@ -30,5 +34,24 @@ var $carousels = $('.carousels').flickity({
     contain: true,
     autoPlay: true,
     pauseAutoPlayOnHover: true,
+
+});
+
+// MAGNIFIC POPUP HERE
+
+$(document).ready(function() {
+
+    $('.rows').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+
+    });
+
+
 
 });
