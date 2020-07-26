d553ec8c7d5cee41b923f062a89fa53fdf461217
mayorjay <joseolu4gsm@yahoo.com>
2020-07-26 20:14:31 +0100

    Feat: Display all classes and requests for student

--- app/src/main/java/com/mobileforce/hometeach/ui/home/student/UpcomingClassViewHolderStudentDashBoard.kt
+++ app/src/main/java/com/mobileforce/hometeach/ui/home/student/UpcomingClassViewHolderStudentDashBoard.kt
@@ -13,19 +13,19 @@ import java.util.*
 class UpcomingClassViewHolderStudentDashBoard(var binding: ListItemClassUpcomingParentDashBoardBinding) :
     ViewHolder<Request>(binding.root) {
     override fun bind(element: Request) {
-        val currentDateTime = System.currentTimeMillis()
-        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH-mm", Locale.US)
-        val requestDateTime = dateFormat.parse(element.day + " " + element.from_hour + ":" + element.from_minute)!!.time
+//        val currentDateTime = System.currentTimeMillis()
+//        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH-mm", Locale.US)
+//        val requestDateTime = dateFormat.parse(element.day + " " + element.from_hour + ":" + element.from_minute)!!.time
         with(element) {
-            if (currentDateTime < requestDateTime) {
-                binding.subjectName.text = subject
-                binding.dateTime.text = "$day-$from_hour:$from_minute"
-                binding.tutorName.text = tutor_name
-                binding.tutorSubject.text = "$subject Tutor"
-                binding.tutorImage.loadImage(URL(tutor_pic))
-            } else {
-                Log.d("TAG", "Time passed")
-            }
+            binding.subjectName.text = subject
+            binding.dateTime.text = "$day-$from_hour:$from_minute"
+            binding.tutorName.text = tutor_name
+            binding.tutorSubject.text = "$subject Tutor"
+            binding.tutorImage.loadImage(URL(tutor_pic))
+//            if (currentDateTime < requestDateTime) {
+//            } else {
+//                Log.d("TAG", "Time passed")
+//            }
         }
     }
 }
