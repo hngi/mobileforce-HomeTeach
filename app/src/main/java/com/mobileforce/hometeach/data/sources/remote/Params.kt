8d57018ac996d89eed8b8a367025d810bb9f2167
enyason <enyasonjnr@gmail.com>
2020-07-28 17:40:04 +0100

    add POST method to update tutor profile count

--- app/src/main/java/com/mobileforce/hometeach/data/sources/remote/Params.kt
+++ app/src/main/java/com/mobileforce/hometeach/data/sources/remote/Params.kt
@@ -81,14 +81,17 @@ class Params {
         val desc: String
     )
 
-    data class TutorClassesRequest (val tutor_id: String)
+    data class TutorClassesRequest(val tutor_id: String)
 
-    data class StudentRequest(val id: String,val request_id:String,val action:String)
+    data class StudentRequest(val id: String, val request_id: String, val action: String)
 
     data class StudentID(
         val student_id: String
     )
 
     data class UserWallet(val user: String)
+
+    data class TutorProfileCount(val tutorId: String)
+
 
 }
